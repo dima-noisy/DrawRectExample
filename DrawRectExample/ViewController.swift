4b647adf7205a470b94fa5639bbda3daf9e32a89
Dmitry Poryadin <dporyadin11@gmail.com>
2024-01-26 23:54:57 +0300

    add files commit

--- DrawRectExample/ViewController.swift
+++ DrawRectExample/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  DrawRectExample
-//
-//  Created by Дмитрий Порядин on 25.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var exampleView: ExampleView = {
+        let view = ExampleView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBlue
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Draw Example"
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(exampleView)
+        
+        setupContraints()
     }
-
-
+    
+    private func setupContraints() {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            exampleView.leadingAnchor.constraint(
+                equalTo: safeAreaGuide.leadingAnchor,
+                constant: 20.0
+            ),
+            exampleView.trailingAnchor.constraint(
+                equalTo: safeAreaGuide.trailingAnchor,
+                constant: -20.0
+            ),
+            exampleView.topAnchor.constraint(
+                equalTo: safeAreaGuide.topAnchor,
+                constant: 20.0
+            ),
+            exampleView.heightAnchor.constraint(equalToConstant: 300.0)
+        ])
+    }
 }
-
